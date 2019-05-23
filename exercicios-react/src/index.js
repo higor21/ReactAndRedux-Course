6932aa8b56321c89cccac508bf330fd6202643c2
higor21 <felype.cet.15@gmail.com>
2019-05-23 17:12:02 -0300

    Hello World example

---
+++ exercicios-react/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FirstComponent from './componentes/primeirocomponente'
+
+const element = document.getElementById('root')
+ReactDOM.render(
+    <FirstComponent/>
+, element)
