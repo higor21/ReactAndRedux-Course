bff84d220afc6d10f65c5830c8a8354bafa8e289
higor <felype.cet.15@gmail.com>
2019-07-01 14:16:19 -0300

    change of dashboardReducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-import DashboardReducer from '../dashboard/dashboardReducer'
+import DashboardReducer from '../dashboard/dashboardReducer' 
 import { tabReducer } from '../common/tabs/tabReducer';
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
